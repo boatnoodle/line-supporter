b58ca127451b4963e031cb1ecb51f4d022a6b269
Nattasit Moonchanabaht <33336824+boatnoodle@users.noreply.github.com>
2023-05-05 17:09:47 +0700

    Install env and setup nodemon script

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const functions = require('@google-cloud/functions-framework');
+require("dotenv").config();
+const functions = require("@google-cloud/functions-framework");
+const { ValidateSignature } = require("./utils/validateSignature");
 
 // Register an HTTP function with the Functions Framework
-functions.http('lineWebhookFunction', (req, res) => {
-  // Your code here
+functions.http("lineWebhookFunction", (req, res) => {
+  const validateSignature = new ValidateSignature(req);
+  if (!validateSignature.isSecureRequest()) {
+    res.send(403, "Invalid signature");
+  }
 
-  // Send an HTTP response
-  res.send('OK');
+  res.send("OK");
 });
