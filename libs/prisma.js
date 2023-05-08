599da11d77482ff83af69f3e05a9325e69cd613e
Nattasit Moonchanabaht <33336824+boatnoodle@users.noreply.github.com>
2023-05-08 13:56:06 +0700

    feat: try to add generate script for prisma

--- libs/prisma.js
+++ libs/prisma.js
@@ -2,6 +2,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// if (process.env.NODE_ENV === "development") global.prisma = prisma;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 module.exports = prisma;
